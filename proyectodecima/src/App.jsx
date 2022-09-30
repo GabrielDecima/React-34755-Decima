edf07ac2ca4f8d9d4519727a7d9b1ecfabcde06f
Gabriel Decima <gabrieldecima@gmail.com>
2022-09-30 15:46:12 -0300

    Agregando componentes

---
+++ proyectodecima/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import './componentes/NavBar';
+import NavBar from './componentes/NavBar';
+
+const App = () => {
+    return (
+        <>
+       <NavBar/> 
+        </>
+
+
+  );
+}
+
+export default App;
+
