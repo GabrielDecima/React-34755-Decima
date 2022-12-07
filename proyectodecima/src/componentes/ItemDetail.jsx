9922d7732fe25989b2a892ebc1b1f5218eff50dd
Gabriel Decima <gabrieldecima1028@gmail.com>
2022-12-07 12:44:48 -0300

    Agregando Eventos

--- proyectodecima/src/componentes/ItemDetail.jsx
+++ proyectodecima/src/componentes/ItemDetail.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // Recibimos por parametro el resultado de la busqueda en la base de datos, luego mostramos la información
 // mediante una card de bt.
 
 const ItemDetail = (producto) => {
+    const [cantidad, setcantidad] = useState(1);
+
+const carrito = []
+    const agregarAlCarrito = (prod,cant) => {
+    let indice = carrito.findIndex(producto => producto.id == prod.id)
+    const prodCarrito = {id: producto.producto.id, cantidad: cant}
+
+    if(indice != -1){
+      carrito[indice]= prodCarrito
+    }else{
+      carrito.push(prodCarrito)
+    }
+    console.log(carrito)
+
+    }
+
+
+
+
+
+
+    const cantidadProducto = (operacion) => {
+      if (operacion== "+"){
+        setcantidad(cantidad +1)
+      }  else{
+        setcantidad(cantidad -1)
+      }
+    }
 
     return (
         <>
@@ -16,7 +44,11 @@ const ItemDetail = (producto) => {
                         <h5 className="card-title">{producto.producto.title}</h5>
                         {/* <p className="card-text">Marca: {producto.producto.attributes[0].value_name}</p> */}
                         <p className="card-text">Precio: ${producto.producto.price}</p>
-                        <a href="#" className="btn btn-dark">Comprar</a>
+
+                        <p className='card-text'>{cantidad}</p>
+                        <button className='btn btn-ligth' onClick={()=> cantidadProducto("+")}>+</button>
+                        <button className='btn btn-dark' onClick={()=> cantidadProducto("-")}>-</button>
+                        <a href="#" className="btn btn-dark" onClick={()=> agregarAlCarrito(producto,cantidad)}>Agregar al Carrito</a>
                     </div>
                  </div>
                  
